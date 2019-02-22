f70f16d26c90e6d0cbbf9b35bbda350254a4bc6c
cot-rui <ruiyang.li@toronto.ca>
2019-02-22 09:54:57 -0500

    Made the widget to be off-panel, instead of in-panel
    - Show info window by clicking feature
    - Group features into one info window
    - Rerender feature symbols
    - Show modal if feature not found

--- URLQuery/Widget.js
+++ URLQuery/Widget.js
@@ -3,13 +3,13 @@ define(['dojo/_base/declare',
         'esri/arcgis/utils',
         "esri/geometry/Extent",
         'esri/tasks/query', 
-        'esri/tasks/QueryTask',
+        "esri/layers/FeatureLayer",
         "esri/InfoTemplate",
-        'esri/graphic', 
+        "esri/renderers/SimpleRenderer",
         'esri/geometry/Point',
         "esri/symbols/jsonUtils",
         'jimu/loaderplugins/jquery-loader!https://code.jquery.com/jquery-3.2.1.min.js, https://code.jquery.com/ui/1.12.1/jquery-ui.min.js'],
-function(declare, BaseWidget, ArcgisUtils, Extent, Query, QueryTask, InfoTemplate, Graphic, Point, JsonUtils, $) {
+function(declare, BaseWidget, ArcgisUtils, Extent, Query, FeatureLayer, InfoTemplate, SimpleRenderer, Point, JsonUtils, $) {
   return declare(BaseWidget, {
 
     name: 'URL Query',
@@ -17,16 +17,14 @@ function(declare, BaseWidget, ArcgisUtils, Extent, Query, QueryTask, InfoTemplat
     map: null,
     fieldName: null,
     queryLayer: null,
-    mapLayers: null,
 
     startup: function(){
       var strCode = '<link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">'; 
-      if (typeof Mustache === 'object') { 
-      } else {
-        strCode += '<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mustache.js/3.0.0/mustache.min.js"></script>'
-      }
+      
       $(".appCode").html(strCode);
-
+      $( "#dialog" ).dialog({
+        autoOpen: false
+      });
       // put related layers to array
       if (this.map.itemId) {
         map = this.map;
@@ -42,63 +40,79 @@ function(declare, BaseWidget, ArcgisUtils, Extent, Query, QueryTask, InfoTemplat
 
       var layerName = this.getURLParams('layer');
       var paramValue = "(" + this.getURLParams('value') + ")";
-     
       fieldName = this.getURLParams("field");
-      if (layerName &&  paramValue && fieldName) {
+      queryLayer = this.layers.filter(function(item){return item.name == layerName})[0];
 
-        queryLayer = this.layers.filter(function(item){return item.name == layerName})[0];
+      if (layerName && paramValue && fieldName && queryLayer) {
+        // add layer to map
+        var infoTemplate = new InfoTemplate("Attributes", this.setInfoWindowContent);
+        featureLayer = new FeatureLayer(queryLayer.url,
+        {
+          infoTemplate: infoTemplate,
+          outFields: ["*"],
+          definitionExpression: fieldName + " IN " + paramValue
+        });
 
-        var queryTask = new QueryTask(queryLayer.url);
+        // rerender feature layer
+        var rerenderSymbol = "";
+        $.getJSON(queryLayer.url + "?f=json", function(response) {
+          if (response.geometryType.toLowerCase().indexOf("point") >=0) {
+            rerenderSymbol = {"color":[0,95,162,255],"size":10,"type":"esriSMS","style":"esriSMSCircle"};
+          }
+          if (response.geometryType.toLowerCase().indexOf("polyline") >=0) {
+            rerenderSymbol = {"color":[0,95,162,255],"width":4,"type":"esriSLS","style":"esriSLSSolid"};
+          }
+          if (response.geometryType.toLowerCase().indexOf("polygon") >=0) {
+            rerenderSymbol = {"color":[0,95,162,128],"width":4,"type":"esriSFS","style":"esriSFSSolid"};
+          }
+          featureLayer.setRenderer(new SimpleRenderer(JsonUtils.fromJson(rerenderSymbol)));
+          map.addLayer(featureLayer);
+        })
+
+
+        // query features to zoom to selected features
         var query = new Query();
         query.where = fieldName + " IN " + paramValue;
         query.outSpatialReference = this.map.spatialReference;
         query.returnGeometry = true;
         query.outFields = ["*"];
-
-        queryTask.execute(query, this.findFeatures, this.errorHandler);
-
+        featureLayer.selectFeatures(query, FeatureLayer.SELECTION_NEW, this.findFeatures);
+      } else {
+        $( "#dialog" ).dialog("open");
       } 
     },
 
     findFeatures: function(data) {
-      for (var i = 0; i < data.features.length; i++) {
-        var html = "";
-        // infowindow popup
-        $.each (data.features[i].attributes, function(key, value) {
-          html = html + "<dt class='title'>" + key + "</dt><dd class='detail'>" + (value?value:"") + "</dd>";
-        })
-        html = html + "</dl>";
-
-         // show features on map
-        var graphic = data.features[i];
-        if (data.features[i].geometry.type == "point") {           
-          /*var selectedPoint = {geometry: data.features[i].geometry, "symbol":{"color":[0,0,0,255],"size":10,"type":"esriSMS","style":"esriSMSCircle"}}
-          map.graphics.add(new Graphic(selectedPoint));
-          map.centerAndZoom(new Point(data.features[i].geometry), 20);
-          */
-          graphic.setSymbol(JsonUtils.fromJson({"color":[0,0,0,255],"size":10,"type":"esriSMS","style":"esriSMSCircle"}));
-          graphic.setInfoTemplate(new InfoTemplate("Attributes", html));
-          map.graphics.add(graphic);
-          map.centerAndZoom(new Point(data.features[i].geometry), 20);
+      var centrePoint;
+      if (data.length > 0) {
+        if (data[0].geometry.type == "point") {           
+          centrePoint = new Point(data[0].geometry);
         } 
 
-        if (data.features[i].geometry.type == "polyline") {
-          /*
-          var selectedLine = {geometry: data.features[i].geometry, "symbol":{"color":[0,0,0,255],"width":4,"type":"esriSLS","style":"esriSLSSolid"}};
-          map.graphics.add(new Graphic(selectedLine));
-          var newExtent = new Extent(data.features[i].geometry.getExtent());
-          map.setExtent(newExtent, true);
-          */
-          graphic.setSymbol(JsonUtils.fromJson({"color":[0,0,0,255],"width":4,"type":"esriSLS","style":"esriSLSSolid"}));
-          graphic.setInfoTemplate(new InfoTemplate("Attributes", html));
-          map.graphics.add(graphic);
-          var newExtent = new Extent(data.features[i].geometry.getExtent());
-          map.setExtent(newExtent, true);
-
+        if (data[0].geometry.type == "polyline") {
+          var mid = Math.round(data[0].geometry.paths[0].length/2);
+          centrePoint = new Point({"spatialReference": data[0].geometry.spatialReference, "x": data[0].geometry.paths[0][mid][0], "y": data[0].geometry.paths[0][mid][1]});
         }
 
+        if (data[0].geometry.type == "polygon") {
+          var mid = Math.round(data[0].geometry.rings[0].length/2);
+          centrePoint = new Point({"spatialReference": data[0].geometry.spatialReference, "x": data[0].geometry.rings[0][mid][0], "y": data[0].geometry.rings[0][mid][1]});
+        }
+
+        map.centerAndZoom(centrePoint, 20);
+      } else {
+        $( "#dialog" ).dialog("open");
       }
         
+    },
+
+    setInfoWindowContent: function (feature) {
+      var html = "<dl>";
+      // infowindow popup
+      $.each (feature.attributes, function(key, value) {
+        html = html + "<dt>" + key + "</dt><dd>" + (value?value:"") + "</dd>";
+      })
+      return html + "</dl>";
     },
 
     errorHandler: function(error) {
@@ -106,7 +120,7 @@ function(declare, BaseWidget, ArcgisUtils, Extent, Query, QueryTask, InfoTemplat
     },
 
     getURLParams: function(sParam) {
-      var sPageURL = window.location.search.substring(1);
+      var sPageURL = decodeURIComponent(window.location.search.substring(1));
       var sURLVariables = sPageURL.split('&');
       for (var i = 0; i < sURLVariables.length; i++) {
           var sParameterName = sURLVariables[i].split('=');
